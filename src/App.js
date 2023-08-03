dbd8461610f02a4956097d94883f808ae1b8ace2
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-03 17:55:24 +0900

    add display

--- src/App.js
+++ src/App.js
@@ -2,8 +2,27 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='formContainer'>
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr />
+        <div class="uiForm">
+          <div class="formField">
+            <label>ユーザー名</label>
+            <input type="text" placeholder='ユーザー名' name='username' />
+          </div>
+          <div class="formField">
+            <label>メールアドレス</label>
+            <input type="text" placeholder='メールアドレス' name='mailAddress' />
+          </div>
+          <div class="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder='パスワード' name='password' />
+          </div>
+          <button class="submitButton">ログイン</button>
+        </div>
+      </form>
+    </div>
   );
 }
 
